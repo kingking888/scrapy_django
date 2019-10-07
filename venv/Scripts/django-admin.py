9a91160cc194d1e3e3af2e01afca1220d98c92c2
Tohfa Akib <tohfaakib@my.smccd.edu>
2019-10-07 19:08:13 +0600

    refactored

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\development\Python\professional\scrapy_django\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

b3686b7523058419dbe55a23e690b0296c870e2c
tohfaakib <tohfaakib@my.smccd.edu>
2019-10-13 16:12:32 +0600

    development server db config separated from settings.py

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -85,12 +85,16 @@ WSGI_APPLICATION = 'NewsPaper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+from config.db import DATABASES
+
+DATABASES = DATABASES
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators

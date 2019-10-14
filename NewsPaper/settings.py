deeb72a2d7ad8f50945d5889f8d4461ac2cffa98
tohfaakib <tnishan7@gmail.com>
2019-10-14 06:04:36 +0000

    refactoring

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -142,16 +142,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 

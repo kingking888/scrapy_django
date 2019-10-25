984ea3920159ca18b49a21f78f3b1dafe5a68fd0
Tohfa Akib <tohfaakib@my.smccd.edu>
2019-10-25 18:54:43 +0600

    refactoring

--- run.py
+++ run.py
@@ -6,13 +6,21 @@ def subprocess_cmd(command):
     print(proc_stdout)
 
 
-subprocess_cmd('python manage.py prothomalo_crawl')
-subprocess_cmd('python manage.py kalerkantho_crawl')
-subprocess_cmd('python manage.py ittefaq_crawl')
-subprocess_cmd('python manage.py bdprotidin_crawl')
-subprocess_cmd('python manage.py dhakatribune_crawl')
-subprocess_cmd('python manage.py jugantor_crawl')
-subprocess_cmd('python manage.py jagonews24_crawl')
-subprocess_cmd('python manage.py ntvbd_crawl')
 subprocess_cmd('python manage.py samakal_crawl')
+subprocess_cmd('python manage.py ntvbd_crawl')
+subprocess_cmd('python manage.py jagonews24_crawl')
+subprocess_cmd('python manage.py jugantor_crawl')
+subprocess_cmd('python manage.py dhakatribune_crawl')
+subprocess_cmd('python manage.py bdprotidin_crawl')
+subprocess_cmd('python manage.py ittefaq_crawl')
+subprocess_cmd('python manage.py kalerkantho_crawl')
+subprocess_cmd('python manage.py prothomalo_crawl')
+
+
+
+
+
+
+
+
 

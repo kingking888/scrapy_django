c1f73afddb7c84cb203641e549063714070c8a23
Tohfa Akib <tohfaakib@my.smccd.edu>
2019-10-26 19:27:17 +0600

    ittefaq refactoring

--- scraper/spiders/ittefaq.py
+++ scraper/spiders/ittefaq.py
@@ -57,8 +57,14 @@ class IttefaqSpider(scrapy.Spider):
             description = response.css('.dtl_content_block p::text').extract()
             description = [x.strip() + '\n\n' for x in description]
             description = listToString(description)
-        if 'আরও পড়ুন:' in description:
+        if 'আরও পড়ুন:'in description:
             description = description.replace('আরও পড়ুন:', '')
+        if 'আরও পড়ুন :'in description:
+            description = description.replace('আরও পড়ুন :', '')
+        if 'আরো পড়ুন :' in description:
+            description = description.replace('আরো পড়ুন :', '')
+        if 'আরো পড়ুন:' in description:
+            description = description.replace('আরো পড়ুন:', '')
         item['description'] = description
         item['image'] = 'https://' + self.allowed_domains[0] + response.css('.dtl_img_block img::attr(src)').extract_first()
         item['url'] = response.request.url+'/'

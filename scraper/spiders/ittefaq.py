e21b171d2f33374e5818c4bef1ead160c062d035
tohfaakib <tohfaakib@my.smccd.edu>
2019-10-28 14:16:01 +0600

    ittefaq refactoring

--- scraper/spiders/ittefaq.py
+++ scraper/spiders/ittefaq.py
@@ -59,6 +59,10 @@ class IttefaqSpider(scrapy.Spider):
                 description = description + response.css('.dtl_content_block p::text').extract()
                 description = [x.strip() + '\n\n' for x in description]
                 description = listToString(description)
+            else:
+                description = description + response.css('.dtl_content_block p::text').extract()
+                description = [x.strip() + '\n\n' for x in description]
+                description = listToString(description)
         if 'আরও পড়ুন:'in description:
             description = description.replace('আরও পড়ুন:', '')
         if 'আরও পড়ুন :'in description:

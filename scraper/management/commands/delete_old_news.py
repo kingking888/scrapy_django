7405176c2daba23e3c4dcd2d76a5eae764bcd9e1
tohfaakib <tohfaakib@my.smccd.edu>
2019-10-31 10:25:20 +0600

    command added to delete old data

--- scraper/management/commands/delete_old_news.py
+++ scraper/management/commands/delete_old_news.py
@@ -2,10 +2,11 @@ from django.core.management.base import BaseCommand, CommandError
 from all_news.models import News
 from datetime import datetime, timedelta
 
+days = 10
 
 class Command(BaseCommand):
     help = 'Delete objects older than 10 days'
 
     def handle(self, *args, **options):
-        News.objects.filter(date__lte=datetime.now()-timedelta(days=10)).delete()
-        # self.stdout.write('Deleted objects older than 10 days')
+        News.objects.filter(date__lte=datetime.now()-timedelta(days=days)).delete()
+        print("Deleted items older than {} days".format(days))
